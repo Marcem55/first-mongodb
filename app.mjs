1f222eb79a2b9ee8894ae18182ef1b4a87d15cc2
Marcem55 <malacalzamarcelo@gmail.com>
2024-07-18 15:57:17 -0300

    pagination in mongoose

--- app.mjs
+++ app.mjs
@@ -52,12 +52,47 @@ const createCourse = async () => {
     published: true,
     price: 30,
   });
+  const course6 = new Course({
+    name: "Ruby on rails desde 0",
+    author: "Cristian",
+    tags: ["ruby", "programming"],
+    published: false,
+    price: 5,
+  });
+  const course7 = new Course({
+    name: "Desarrolla en .NET ya!",
+    author: "Cristian",
+    tags: [".NET", "programming", "fullstack"],
+    published: false,
+    price: 50,
+  });
+  const course8 = new Course({
+    name: "Aprende a trabajar con CSS como un PRO!",
+    author: "Franco",
+    tags: ["css", "styles", "design"],
+    published: true,
+    price: 45,
+  });
+  const course9 = new Course({
+    name: "R, la base de todo",
+    author: "Juampy",
+    tags: ["R", "programming"],
+    published: true,
+    price: 28,
+  });
+  const course10 = new Course({
+    name: "Angular, el framework de tu vida",
+    author: "Julieta",
+    tags: ["angular", "programming", "fronted"],
+    published: true,
+    price: 5,
+  });
 
-  const courseResult = await course.save();
-  const courseResult2 = await course2.save();
-  const courseResult3 = await course3.save();
-  const courseResult4 = await course4.save();
-  const courseResult5 = await course5.save();
+  const courseResult = await course6.save();
+  const courseResult2 = await course7.save();
+  const courseResult3 = await course8.save();
+  const courseResult4 = await course9.save();
+  const courseResult5 = await course10.save();
   console.log(courseResult);
   console.log(courseResult2);
   console.log(courseResult3);
@@ -68,6 +103,7 @@ const createCourse = async () => {
 // createCourse();
 
 const getCourses = async () => {
+  //! Métodos de búsqueda
   //* find() devuelve todos los documentos del modelo especificado
   //* dentro del find, podemos buscar especificamente documentos donde, en este caso, la propiedad published === true. Podríamos buscar en base a cualquier propiedad que queramos
   //* limit() lo utilizamos para limitar justamente la cantidad de registros que nos devuelve
@@ -80,9 +116,9 @@ const getCourses = async () => {
       name: 1,
       tags: 1,
     });
-  console.log("courses1", courses);
+  //   console.log("courses1", courses);
 
-  //? Operadores de comparación
+  //! Operadores de comparación
   //* eq (equal)
   //* ne (not equal)
   //* gt (greather than)
@@ -96,9 +132,9 @@ const getCourses = async () => {
     // .find({ price: { $in: [10, 15, 30] } })
     // .find({ price: { $ne: 20 } })
     .find({ price: { $lt: 20 } });
-  console.log("courses2", courses2);
+  //   console.log("courses2", courses2);
 
-  //? Operadores lógicos
+  //! Operadores lógicos
   //* or()
   //* and()
   const courses3 = await Course.find()
@@ -116,16 +152,32 @@ const getCourses = async () => {
       },
       { published: true },
     ]);
-  console.log("courses3", courses3);
+  //   console.log("courses3", courses3);
 
-  //? Expresiones regulares
+  //! Expresiones regulares
   //* Empiece con la palabra Mar /^Mar/
   //   const courses4 = await Course.find({ author: /^Ju/ });
   //* Termine con la palabra celo /celo$/
   //   const courses4 = await Course.find({ author: /celo$/ });
   //* Contenido específico en algún campo /.*ar.*/
   const courses4 = await Course.find({ name: /.*ana.*/ });
-  console.log("courses4", courses4);
+  //   console.log("courses4", courses4);
+
+  //? Paginación
+  //* skip()
+  const page = 2;
+  const limit = 5;
+  const courses5 = await Course.find()
+    .limit(limit)
+    .skip((page - 1) * limit);
+
+  const count = await Course.countDocuments();
+  console.log("courses5", {
+    courses5,
+    totalPages: Math.ceil(count / limit),
+    currentPage: page,
+    resultsInThisPage: courses5.length,
+  });
 };
 
 getCourses();
